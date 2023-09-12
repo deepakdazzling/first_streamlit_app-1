0d67531ed0f896b9c5fc8d704b5c7a5e52837e58
Deepaksarans <144823258+Deepaksarans@users.noreply.github.com>
2023-09-12 23:02:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.Header ('Breakfast Menu')
 streamlit.text ('Omega 2 & Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
